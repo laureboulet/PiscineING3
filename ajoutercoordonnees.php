09a7901cf8cbe72f30efb5f3045a096df5ed3af3
laureboulet <49633566+laureboulet@users.noreply.github.com>
2020-04-14 19:58:20 +0200

    Add files via upload

--- ajoutercoordonnees.php
+++ ajoutercoordonnees.php
@@ -16,29 +16,31 @@ $db_found = mysqli_select_db($db_handle, $database);
 
 if($_POST["button3"]){
     if($db_found){
-       /* $sql = "SELECT * FROM coord";
+        $sql = "SELECT * FROM coord";
         if($email != "") {
             $sql .= " WHERE Email LIKE '%$email%'";
             if($mdp != ""){
-                $sql .= " AND Mdp LIKE '%$mdp%'";
-
+                $sql .= " AND Telephone LIKE '%$telephone%'";
+                if($mdp != ""){
+                $sql .= " AND Adresse LIKE '%$adresse%'";
+                }
             }
-        } */
-      //  $result = mysqli_query($db_handle, $sql);
+        } 
+        $result = mysqli_query($db_handle, $sql);
    
-   /* if (mysqli_num_rows($result) != 0){
-    echo "Vous avez déjà créé un compte. Connectez-vous directement sur notre site !";
-    header('Location:loginacheteur.html');
-    } */
-    //else{
-    $sql = "INSERT INTO coord(Adresse, Ville, CP, Pays, Email, Telephone) VALUES('$adresse', '$ville', '$cp', '$pays', '$email', 'telephone')";
+    if (mysqli_num_rows($result) != 0){
+    echo "Ces coordonnées correspondent déjà à un compte vous avez surement fait une erreur !";
+    header('Location:loginacheteur3.html');
+    } 
+    else{
+    $sql = "INSERT INTO coord(Adresse, Ville, CP, Pays, Email, Telephone) VALUES('$adresse', '$ville', '$cp', '$pays', '$email', '$telephone')";
     $result = mysqli_query($db_handle, $sql);
 
     //vérification de la création de compte
     echo "Félicitations ! Vos coordonnées ont bien été enregistrées !";
-    //diréction vers la prochaine page html (suite de création de compte)
+    //diréction vers la prochaine page html (page d'accueil achats)
     header('Location:achat.html');
-
+    }
     }
 
     
