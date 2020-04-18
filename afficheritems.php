867ec3b42f1451284ea033f3f2d846f057c57ddc
Nivine20 <47658193+Nivine20@users.noreply.github.com>
2020-04-19 01:45:40 +0200

    Add files via upload

--- afficheritems.php
+++ afficheritems.php
@@ -1,135 +1,208 @@
 <?php
-session_start();
-include("ajouteritem.php");
-include("supprimeritem.php");
-//Identification de la BDD
-$database = "piscineweb";
-//Connexion à la BDD
-$db_handle = mysqli_connect('localhost', 'root', 'root');
-$db_found = mysqli_select_db($db_handle, $database);
+  session_start();
+  $database = "piscineweb";
+  //connectez-vous de votre BDD
+  $db_handle = mysqli_connect('localhost', 'root', 'root'); 
+  $db_found = mysqli_select_db($db_handle, $database);
+  $nomitem = isset($_POST["nomitem"])? $_POST["nomitem"] : "";
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Afficher item</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="vendreitem.css">
-	<script type="text/javascript">
-
-        $(document).ready(function()
-            { $('.header').height($(window).height());
-            });
-	</script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Supprimer des items par le vendeur</title>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="item.css">
 </head>
+
 <body>
-		<!--Barre de navigation/menu-->
-	<nav class="navbar navbar-expand-md">
-      	<img src="logo.png" alt="Logo" width="130" height="100"/>
-      	<button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation"> <span class="navbar-toggler-icon"></span>
-      	</button>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-md fixed-top">
+    <img src="logo.png" alt="Logo" width="130" height="100"/>
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation"> <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="main-navigation">
+          <ul class="navbar-nav">
+              <li class="nav-item"><a class="nav-link" href="accueilvendeur.php"><strong>Accueil</strong></a></li>
+              <li class="nav-item"><a class="nav-link" href="afficheritem.php"><strong>Items en vente</strong></a></li>
+              <li class="nav-item"><a class="nav-link" href="vendreItem.php"><strong>Vendre un item</strong></a></li>
+              <li class="nav-item"><a class="nav-link" href="decovendeur.php"><strong>Se déconnecter</strong></a></li>
+              <li class="nav-item-compte"><a class="nav-link-compte" href="#"><strong><?php echo $_SESSION['Pseudovend'];?></strong></a></li>
+          </ul>
+          <ul class="navbar-nav">
 
-    	<div class="collapse navbar-collapse" id="main-navigation">
-        	<ul class="navbar-nav">
-          		<li class="nav-item"><a class="nav-link" href="accueilvendeur.php"><strong>Accueil</strong></a></li>
-          		<li class="nav-item"><a class="nav-link" href="afficheritems"><strong>Items en vente</strong></a></li>
-          		<li class="nav-item"><a class="nav-link" href="vendreItem.php"><strong>Vendre un item</strong></a></li>
-          		<li class="nav-item-compte"><a class="nav-link-compte" href="#"><strong></strong></a></li>
-        	</ul>
-        	 <ul class="navbar-nav">
-                <img src="profil.png" id="profil" alt="Profil" width="100" height="100" border-radius="50">
+                    <?php echo'<img src="profil.png" id="profil" alt="Profil" width="100" height="100" border-radius="50">';?>
+
             </ul>
+      </div>
+  </nav>
+  <!-- Page Content -->
+  <div class="container" style="padding-top: 100px">
+    <div class="row"> 
+      <div class="col-lg-3">
+        <div class="list-group" style="padding-top: 30px">
+          <form method="post" action="afficheritems.php" class="item">
+              <p>Quel article voulez-vous supprimer ? <br>Veuillez-nous indiquer votre choix en écrivant son nom</p>
+              <input type="text" name="nomitem">
+              <input type="submit" name="supprimer" value="Supprimer">
+          </form>
         </div>
-    </nav>
-    <header class="page-header header container-fluid">
-    	
-    	<!--Nom image + image-->
-    	    	<div class="container">
-	    				<div class="row">
-	    					<div class="col-lg-6 col-md-6 col-sm-12">
-    							<h3><strong>
-									<?php $sql = "SELECT * FROM item";
-
-							    	$result = mysqli_query($db_handle, $sql);
-							    		while ($data = mysqli_fetch_assoc($result)) {
-							        	$image=$data['Photo1'];
-										echo "Nom: " . $data['Nomitem'] . "<br>";?></strong></h3><br>
-
-									<form method="post" method="post" action="supprimeritem.php">
-									<td colspan="2" align="left">
-									<input type="submit" name="vendreItem2" id="boutton2" value="Supprimer item" /></td></form>
-									
-								<h3><strong><?php echo "<img src='$image' width='200', height='200'>". "<br>";}?></strong></h3><br>
-							</div>
-		<!--Description-->	
-							<div class="col-lg-6 col-md-6 col-sm-12">
-
-								
-								<div class="propriete">
-								<p><?php 
-								$sql = "SELECT * FROM item";
-             					$result = mysqli_query($db_handle, $sql);
-        						while ($data = mysqli_fetch_assoc($result)) {
-
-
-            					$image=$data['Photo1'];
-        
-                    			echo "Description: " . $data['Description'] . "<br>";
-                    			echo "Categorie: " . $data['Categorie'] . "<br>";
-               
-                    			echo "Typeachat: " . $data['Typeachat'] . "<br>" ."<br>";} ?></p>
-                    		</div>  
-							</div>
-
-    </header>
-
-    <footer class="page-footer text-center"> 
-		<div id="nav">
-            <a class="link" href="loginvendeur.php">Login vendeur |</a>
-            <a class="link" href="loginacheteur.php">Login acheteur |</a>
-            <a class="link" href="#">Login administrateur |</a>
+        <?php
+          if($_POST["supprimer"]){
+            if($db_found){
+              $sql = "SELECT * FROM item";
+              if ($nomitem != "") {
+                $sql .= " WHERE Nomitem LIKE '%$nomitem%'";
+              }
+              $result = mysqli_query($db_handle, $sql);
+              if (mysqli_num_rows($result) == 0){
+                echo("L'item n'existe pas.");
+              }
+              else{
+                while ($data = mysqli_fetch_assoc($result)) {
+                  $iditem = $data['Iditem'];
+                  //echo "<br>";
+                }
+                  $sql = "DELETE FROM item";
+                  $sql .= " WHERE Iditem = $iditem";
+                  $result = mysqli_query($db_handle, $sql); 
+                  echo "Item supprimé. <br>";
+              }
+            }
+            else{
+              echo "Database not found";
+            }
+          }
+        ?>
+      </div>
+      <!-- /.col-lg-3 -->
+      <div class="col-lg-9">
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="ferraille.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="musee.png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="vip.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <div class="row">
+        <?php
+          if ($db_found) {
+            $sql = "SELECT * FROM item"; 
+            $result = mysqli_query($db_handle, $sql);
+            if (mysqli_num_rows($result) == 0) {
+              echo "Pas d'item à vendre."; 
+            } 
+            else {
+              while ($data = mysqli_fetch_assoc($result)) {                   
+          ?>
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <?php $image = $data['Photo1']; ?>
+              <a href="#"><?php echo "<img class='card-img-top' src='$image'>"; ?></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#" style="font-size: 20px; color:#7EC6A3"> <?php echo $data['Nomitem']; ?></a><br>
+                  <a href="#" style="font-size: 15px; color:#7EC6A3">Item <?php echo $data['Iditem']; ?></a>
+                </h4>
+                <h5><?php echo $data['Prixitem'];?>€</h5>
+                <p class="card-text"><?php echo $data['Description']; ?></p>
+              </div>
+              <div class="card-footer">
+                <?php echo $data['Categorie'];?><br>
+                <small class="text-muted"><?php echo $data['Typeachat'];?></small>
+              </div>
+            </div>
+          </div>
+          <?php  
+              }
+            }
+          } 
+          else {
+            echo "Database not found";
+          }
+          ?>
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.col-lg-9 -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+  <!-- Footer -->
+  <footer class="page-footer text-center"> 
+    <div id="nav">
+            <a class="link" href="accueiladmin.html">Accueil |</a>
+            <a class="link" href="gereritem.php">Items en vente |</a>
+            <a class="link" href="gerervendeur.php">Vendeurs |</a>
+            <a class="link" href="#">Enchères</a>
         </div>
         <br>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4 col-md-4 col-sm-12">
-					<h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">A propos de nous</h6> 
-					<h7><a class="info" href="#">Qui sommes-nous?</a></h7><br>
-					<h7><a class="info" href="#">Notre histoire</a></h7>
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-12">
-					<h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">Informations légales</h6>
-					<h7><a class="info" href="#">Conditions générales d'utilisation</a></h7><br>
-					<h7><a class="info" href="#">Conditions générales de vente</a></h7><br>
-					<h7><a class="info" href="#">Vie privée / Cookies</a></h7>
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-12">
-					<h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">Nous contactez</h6>
-					<p>
-					Ebay ECE <br>
-					37, quai de Grenelle, <br>
-					75015 Paris, France <br> 
-					<a style="color:#7EC6A3" href="mailto:ebayece@edu.ece.fr">ebayece@edu.ece.fr</a>
-					</p> 
-				</div>
-				<div>
-					<h6 class="text-uppercase font-weight-bold">Retrouvez-nous sur</h6>
-					<img src="facebook.png" alt="facebook" width="20" height="20"/>
-					<img src="instagram.png" alt="instagram" width="20" height="20"/>
-					<img src="twitter.png" alt="twitter" width="20" height="20"/>
-					<img src="linkedin.png" alt="linkedin" width="20" height="20"/>
-				</div>
-			</div>
-			<div style="color:#7EC6A3" id="footer-copyright">N°1 de site de vente en ligne
-			</div> 
-		</div>
-	</footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">A propos de nous</h6> 
+          <h7><a class="info" href="#">Qui sommes-nous?</a></h7><br>
+          <h7><a class="info" href="#">Notre histoire</a></h7>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">Informations légales</h6>
+          <h7><a class="info" href="#">Conditions générales d'utilisation</a></h7><br>
+          <h7><a class="info" href="#">Conditions générales de vente</a></h7><br>
+          <h7><a class="info" href="#">Vie privée / Cookies</a></h7>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <h6 style="border-bottom: 2px solid #000000" class="text-uppercase font-weight-bold">Nous contactez</h6>
+          <p>
+          Ebay ECE <br>
+          37, quai de Grenelle, <br>
+          75015 Paris, France <br> 
+          <a style="color:#7EC6A3" href="mailto:ebayece@edu.ece.fr">ebayece@edu.ece.fr</a>
+          </p> 
+        </div>
+        <div>
+          <h6 class="text-uppercase font-weight-bold">Retrouvez-nous sur</h6>
+          <img src="facebook.png" alt="facebook" width="20" height="20"/>
+          <img src="instagram.png" alt="instagram" width="20" height="20"/>
+          <img src="twitter.png" alt="twitter" width="20" height="20"/>
+          <img src="linkedin.png" alt="linkedin" width="20" height="20"/>
+        </div>
+      </div>
+      <div style="color:#7EC6A3" id="footer-copyright">N°1 de site de vente en ligne
+      </div> 
+    </div>
+  </footer>
 
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
