d04cb57ca9c8f1047eae54ee11e20da27f6d40a4
bourgat <47519287+bourgat@users.noreply.github.com>
2020-04-18 17:22:33 +0200

    Update achat.php

--- achat.php
+++ achat.php
@@ -14,7 +14,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Supprimer des items par l'administrateur</title>
+  <title>Achat</title>
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom styles for this template -->
